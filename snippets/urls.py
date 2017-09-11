c3944b33566e55817aaae2b14f6b30f14fbe5dc6
jakobpederson <jakob.pederson+github@gmail.com>
2017-09-11 15:40:25 -0500

    Routers

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,39 +1,14 @@
 from django.conf.urls import url, include
-from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework import renderers
+
+from rest_framework.routers import DefaultRouter
 
 from snippets import views
 
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
 
-snippet_list = views.SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create',
-})
-snippet_detail = views.SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy',
-})
-snippet_highlight = views.SnippetViewSet.as_view({
-    'get': 'highlight',
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-user_list = views.UserViewSet.as_view({
-    'get': 'list',
-})
-user_detail = views.UserViewSet.as_view({
-    'get': 'retrieve',
-})
-
-urlpatterns = format_suffix_patterns([
-    url(r'^snippets/$', snippet_list, name='snippet-list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail, name='snippet-detail'),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', snippet_highlight, name='snippet-highlight'),
-    url(r'^users/$', user_list, name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
-    url(r'^$', views.api_root),
-])
-
-urlpatterns += [
-    url(r'^api-auth/', include('rest_framework.urls', namespace='restframework')),
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
